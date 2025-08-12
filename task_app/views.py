dfc7fc9dc948fc6cd7d47d6e8d36b300cdd9d1cf
General202 <user.den.ua@gmail.com>
2025-08-12 20:08:51 +0300

    add logout

--- task_app/views.py
+++ task_app/views.py
@@ -5,19 +5,22 @@ from django.urls import reverse_lazy
 
 from task_app.forms import TaskForm
 from .models import Task # Припустимо, у вас є модель Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class TaskListView(ListView):
+class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'task_list.html'
     context_object_name = 'tasks'
+    def get_queryset(self):
+        queryset = Task.objects.filter(created_by = self.request.user)
+        return queryset
 
-
-class TaskDetailView(DetailView):
+class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'task_detail.html'
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     template_name = 'task_form.html'
     success_url = reverse_lazy('task_list')
@@ -26,9 +29,9 @@ class TaskCreateView(CreateView):
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
-    
-    
-class TaskUpdateView(UpdateView):
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     template_name = 'task_update.html'
     success_url = reverse_lazy('task_list')
@@ -37,9 +40,9 @@ class TaskUpdateView(UpdateView):
     def form_valid(self, form):
         form.instance.updated_by = self.request.user
         return super().form_valid(form)
-    
-    
-class TaskDeleteView(DeleteView):
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     template_name = 'task_delete.html'
     success_url = reverse_lazy('task_list')
